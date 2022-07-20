9c11a8fd7bf2adbf0ddcd6bd9c471869080bb0f5
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-20 09:04:11 +0200

    Generata funzione per password randomica, da implementare

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,41 @@ const lowerCase = document.getElementById("p-lowercase") // Questo è ID lowerca
 const pNumber = document.getElementById("p-number") // Questo è ID dei numeri
 const pSymbol = document.getElementById("p-symbol") // Questo è ID dei simboli
 const submitButton = document.getElementById("submit") // Questo è ID del submit button
+
+const password = document.getElementById("password") // Questo è ID della password
+
+submitButton.addEventListener("click", () => {
+    /* console.log("Sono il tasto submit e sono stato appena cliccato"); */
+    let initial = empty // Inizialmente la password sarà vuota (empty è costante dichiarata sopra)
+        /* Analisi condizione
+            SE upperCase(form control) è selezionato (check dell'input checkbox)
+            Allora initial si somma a se stessa (essendo vuota) + un valore di uCase (random di lettere uppercase)
+        */
+        (upperCase.checked) ? initial += uCase : "";
+    /* Ora replico la stessa condizione per tutti gli altri check */
+    (lowerCase.checked) ? initial += lCase : ""; // Condizione Lowercase
+    (pNumber.checked) ? initial += number : ""; // Condizione Numeri
+    (pSymbol.checked) ? initial += symbol : ""; // Condizione Simboli
+
+    /* Valore della password generata tramite una funzione invocata */
+    password.value = generatePassword(pLength.value, initial)
+})
+
+/**
+ * Funzione necessaria per la generazione della password
+ * @param {string} l Lunghezza della password
+ * @param {*} initial condizione iniziale della password
+ */
+function generatePassword(l, initial) {
+    let pass = "" // Stringa inizialmente vuota
+    /* Avvio un ciclo per le numeri, lettere etc randomici */
+    /* Analisi ciclo
+        Finchè i è minore del valore lunghezza password (pLength.value)
+        la variabile pass, inizialmente vuota avrà metodo randomico (Math) per generare la pass
+    */
+    for (let i = 0; i < l; i++) {
+        pass += initial.charAt(Math.floor(Math.random() * initial.length));
+    }
+    return pass // Ritorno il valore della password
+}
+
