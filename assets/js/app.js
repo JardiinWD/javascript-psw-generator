e273600ca4c55ddc2f1da95610c17687643bcaf4
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-20 08:12:27 +0200

    Iniziato a identificare in JS costanti relative a id markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-/* alert("Ciao") */
+const empty = ""; // Stringa inizialmente vuota che conterrà i caratteri randomici
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Lettere randomiche in uppercase
+const lCase = "abcdefghijklmnopqrstuvwxyz"; // Lettere randomiche in lowercase
+const number = "0123456789"; // numeri randomici
+const symbol = "!@#$%^&£*=-_'"; // simboli randomici
+
+const pLength = document.getElementById("p-length") // Questo è ID di lunghezza password
+const upperCase = document.getElementById("p-uppercase") // Questo è ID uppercase
+const lowerCase = document.getElementById("p-lowercase") // Questo è ID lowercase
+const pNumber = document.getElementById("p-number") // Questo è ID dei numeri
+const pSymbol = document.getElementById("p-symbol") // Questo è ID dei simboli
+const submitButton = document.getElementById("submit") // Questo è ID del submit button
